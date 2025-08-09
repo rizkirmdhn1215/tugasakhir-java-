0ff35f19664473b331474ca2eb3c50faa8a78a52
rizkirmdhn1215 <rizkirmdhn1215@gmail.com>
2025-08-09 16:55:52 +0700

    new

---
+++ src/main/java/com/sttp/skripsi/controller/HealthController.java
@@ -0,0 +1,19 @@
+package com.sttp.skripsi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/actuator/health")
+    public ResponseEntity<Map<String, String>> health() {
+        Map<String, String> status = new HashMap<>();
+        status.put("status", "UP");
+        return ResponseEntity.ok(status);
+    }
+}
